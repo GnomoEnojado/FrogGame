0a254d669c25091144fa2311269dccba9fc93f7f
Maury <mauryxd.gonz@gmail.com>
2018-03-12 22:06:45 -0300

    Corregido error en la rotacion del personaje

--- PrototipoFrog/Assets/Scripts/Player/CPlayer.cs
+++ PrototipoFrog/Assets/Scripts/Player/CPlayer.cs
@@ -34,8 +34,23 @@ public class CPlayer : MonoBehaviour {
     void Update () {
         timer += Time.deltaTime;
 
-        
-        
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            transform.eulerAngles = new Vector3(0, 90, 0);
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            transform.eulerAngles = new Vector3(0, -90, 0);
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            transform.eulerAngles = new Vector3(0, 180, 0);
+        }
 
         if (gameObject.transform.position==endPos)
         {
@@ -44,24 +59,10 @@ public class CPlayer : MonoBehaviour {
 				firstInput = true;
 				_preJump = true;
 
-				if (Input.GetKeyDown(KeyCode.W))
-				{
-					transform.eulerAngles = new Vector3(0, 90, 0);
-				}
-				else if (Input.GetKeyDown(KeyCode.S))
-				{
-					transform.eulerAngles = new Vector3(0, -90, 0);
-				}
-				else if (Input.GetKeyDown(KeyCode.A))
-				{
-					transform.eulerAngles = new Vector3(0, 0, 0);
-				}
-				else if (Input.GetKeyDown(KeyCode.D))
-				{
-					transform.eulerAngles = new Vector3(0, 180, 0);
-				}
+				
 			}
-			if (Input.GetKeyUp(KeyCode.A)|| Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.S))
+          
+            if (Input.GetKeyUp(KeyCode.A)|| Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.S))
 			{
 				time=0;
 				_preJump = false;
